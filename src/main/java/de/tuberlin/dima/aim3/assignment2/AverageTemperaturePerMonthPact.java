2a5e113f8404dbedc5f7e96e80fcc4d2edbd5ac6
Thomas Bodner <tbodner@tbodner-x220.(none)>
2012-05-23 18:05:37 +0200

    Fixed unit  tests for assignment 3

--- src/main/java/de/tuberlin/dima/aim3/assignment2/AverageTemperaturePerMonthPact.java
+++ src/main/java/de/tuberlin/dima/aim3/assignment2/AverageTemperaturePerMonthPact.java
@@ -1,5 +1,7 @@
 package de.tuberlin.dima.aim3.assignment2;
 
+import eu.stratosphere.pact.common.io.TextInputFormat;
+import eu.stratosphere.pact.common.io.TextOutputFormat;
 import eu.stratosphere.pact.common.plan.Plan;
 import eu.stratosphere.pact.common.plan.PlanAssembler;
 import eu.stratosphere.pact.common.plan.PlanAssemblerDescription;
@@ -7,15 +9,18 @@ import eu.stratosphere.pact.common.stub.Collector;
 import eu.stratosphere.pact.common.stub.MapStub;
 import eu.stratosphere.pact.common.stub.ReduceStub;
 import eu.stratosphere.pact.common.type.Key;
+import eu.stratosphere.pact.common.type.KeyValuePair;
 import eu.stratosphere.pact.common.type.base.PactDouble;
 import eu.stratosphere.pact.common.type.base.PactInteger;
 import eu.stratosphere.pact.common.type.base.PactNull;
+import eu.stratosphere.pact.common.type.base.PactPair;
 import eu.stratosphere.pact.common.type.base.PactString;
 
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
 import java.util.Iterator;
+import java.util.StringTokenizer;
 
 public class AverageTemperaturePerMonthPact implements PlanAssembler, PlanAssemblerDescription {
 
@@ -32,12 +37,46 @@ public class AverageTemperaturePerMonthPact implements PlanAssembler, PlanAssemb
     return null;
   }
 
+  public static class TemperatureInFormat extends TextInputFormat<PactNull, PactString> {
+
+		@Override
+		public boolean readLine(KeyValuePair<PactNull, PactString> pair, byte[] line) {
+			pair.setKey(new PactNull());
+			pair.setValue(new PactString(new String(line)));
+			return true;
+		}
+	}
+  
+  public static class TemperatureOutFormat extends TextOutputFormat<YearMonthKey, PactDouble> {
+
+		@Override
+		public byte[] writeLine(KeyValuePair<YearMonthKey, PactDouble> pair) {
+			String key = pair.getKey().toString();
+			String value = pair.getValue().toString();
+			String line = key + "\t" + value + "\n";
+			return line.getBytes();
+		}
+	}
+  
   public static class TemperaturePerYearAndMonthMapper
       extends MapStub<PactNull, PactString, YearMonthKey, PactInteger> {
 
     @Override
     public void map(PactNull pactNull, PactString line, Collector<YearMonthKey, PactInteger> collector) {
-      //IMPLEMENT ME
+
+    	double minimumQuality = 0.25;
+		String measurement = line.toString();
+		
+		StringTokenizer tokenizer = new StringTokenizer(measurement);
+		while (tokenizer.hasMoreElements()) {
+			int year = (Integer) tokenizer.nextElement();
+			int month = (Integer) tokenizer.nextElement();
+			int temperature = (Integer) tokenizer.nextElement();
+			double quality = (Double) tokenizer.nextElement();
+			if (quality >= minimumQuality) {
+				collector.collect(new YearMonthKey(new PactInteger(year), new PactInteger(month)), new PactInteger(temperature));
+			}
+		}
     }
   }
 
@@ -47,34 +86,30 @@ public class AverageTemperaturePerMonthPact implements PlanAssembler, PlanAssemb
     @Override
     public void reduce(YearMonthKey yearMonthKey, Iterator<PactInteger> temperatures,
         Collector<YearMonthKey, PactDouble> collector) {
-      // IMPLEMENT
+    	int count = 0;
+		int sum = 0;
+		while (temperatures.hasNext()) {
+			count++;
+			PactInteger element = (PactInteger) temperatures.next();
+			sum += element.getValue();
+		}
+		double average = (double) sum / (double) count;
+		collector.collect(yearMonthKey, new PactDouble(average));
     }
   }
+  
+	public static class YearMonthKey extends PactPair<PactInteger, PactInteger> {
 
-  public static class YearMonthKey implements Key {
+		public YearMonthKey() {
+			super();
+		}
 
-    public YearMonthKey() {}
+		public YearMonthKey(PactInteger i1, PactInteger i2) {
+			super(i1, i2);
+		}
 
-    public YearMonthKey(short year, short month) {
-      //IMPLEMENT
-    }
-
-    @Override
-    public int compareTo(Key other) {
-      // IMPLEMENT
-      return 0;
-    }
-
-    @Override
-    public void write(DataOutput out) throws IOException {
-      //IMPLEMENT
-    }
-
-    @Override
-    public void read(DataInput in) throws IOException {
-      //IMPLEMENT
-    }
-
-    //IMPLEMENT equals() and hashCode()
-  }
+		public String toString() {
+			return getFirst().toString() + "\t" + getSecond().toString();
+		}
+	}
 }
